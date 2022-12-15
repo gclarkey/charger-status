50ec0cede13f171d3cb1f49c5b8b0cd0302f0b5c
garethclarke <gareth.clarke@surecert.com>
2022-12-15 11:34:00 +0000

    updated to loop a set amount of times. will only alert when a charger is available.

--- charger-status.py
+++ charger-status.py
@@ -1,34 +1,69 @@
-import requests
+#!/usr/local/bin/python3
+import logging
 import os
+import time
 
+import requests
 
 # park and ride
 chargerId = '2104'
 # the ivanhoe
 # chargerId = '608'
-
 url = 'https://myaccount.esbecars.com/stationFacade/findStationById?stationId=' + chargerId
-response = requests.get(url)
-stationSockets = response.json()['data']['stationSockets']
 
-available = False
+hours = 10
+minutes = 60
+timesToTry = hours * minutes
 
-for stationSocket in stationSockets:
-    print(stationSocket['socketStatusId'])
-    socketStatus = stationSocket['socketStatusId']
-    if socketStatus == 'AVAILABLE':
-        available = True
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def notify(title, text):
+def get_sockets():
+    response = requests.get(url)
+    # print(json.dumps(response.json(), indent=2))
+    return response.json()['data']['stationSockets']
+
+
+def determine_availability(sockets):
+    available = False
+    for stationSocket in sockets:
+        name = stationSocket['name']
+        socket_status = stationSocket['socketStatusId']
+        start_time = stationSocket['chargeTransactionStartTime']
+        minutes_since_start = int((time.time() - start_time / 1000) / 60)
+        logging.info(
+            'Charger: {} is {}. Session started {} minutes ago'.format(name, socket_status, minutes_since_start))
+
+        if socket_status == 'AVAILABLE':
+            available = True
+
+    return available
+
+
+def notify(available):
+    if available:
+        logging.info('Charger ' + chargerId + ' is available')
+        macos_notify("Charger Available", 'Charger ' + chargerId + ' is available')
+    else:
+        logging.info('Charger is not available')
+        # macos_notify("Charger Busy", 'Charger ' + chargerId + ' is busy')
+
+
+def macos_notify(title, text):
     # os.system("""osascript -e 'display notification "{}" with title "{}"'""".format(text, title))
     # os.system("""osascript -e 'tell app "Finder" to display dialog "Hello World"'""".format(text, title))
     os.system("""osascript -e 'tell app "System Events" to display dialog "{}" with title "{}"'""".format(text, title))
 
 
-if available:
-    print('Charger ' + chargerId + ' is available')
-    notify("Charger Available", 'Charger ' + chargerId + ' is available')
-else:
-    print('Charger is not available')
+def do_work():
+    for i in range(timesToTry):
+        sockets = get_sockets()
+        available = determine_availability(sockets)
+        notify(available)
+        time.sleep(60)
 
+
+do_work()
